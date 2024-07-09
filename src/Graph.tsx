eedd9c0cb2fbe709864f9180efcf92123c64380d
Mittapally-vani <134047013+Mittapally-vani@users.noreply.github.com>
2024-07-09 11:07:16 +0530

    Graph.js

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -23,10 +23,13 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      triggr=er_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -38,11 +41,14 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('view', 'y_line');
       elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio","lower_bound", "upper_bound", trigger_alert]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def:'avg',
+        ratio: 'avg',
+        upper_bound:'avg',
+        lower_bound:'avg',
+        trigger_alert: 'avg',
         timestamp: 'distinct count',
       }));
     }
@@ -50,8 +56,9 @@ class Graph extends Component<IProps, {}> {
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
+        ] as unknown as tableData);
       );
     }
   }
